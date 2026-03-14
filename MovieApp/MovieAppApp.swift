55f19d8aad1d52128fefcb378dd9df376f3894a1
Aryan Verma <varyan069@gmail.com>
2026-03-14 16:35:04 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Aryan Verma on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

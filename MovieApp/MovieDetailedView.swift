114aa33c67fbd975bcc4efcde80645100eef5539
SynxGentox <varyan069@gmail.com>
2026-03-29 01:34:53 +0530

    Update MovieDetailedView.swift

--- MovieApp/MovieDetailedView.swift
+++ MovieApp/MovieDetailedView.swift
@@ -13,63 +13,62 @@ struct MovieDetailedView: View {
 
     var body: some View {
         ScrollView(.vertical) {
-                if store.status == .loading {
-                    ProgressView()
-                }
-                else if store.status == .success {
-                    MoviePoster(imageURL: movie.Poster)
-                        .background(.ultraThinMaterial)
-                        .frame(maxWidth: .infinity, maxHeight: 300)
+            if store.status == .loading {
+                ProgressView()
+            }
+            else if store.status == .success {
+                MoviePoster(imageURL: movie.Poster)
+                    .background(.ultraThinMaterial)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
                 
-                    Text(movie.Title)
-                        .font(
-                            .system(size: 34, weight: .semibold, design: .serif)
-                        )
+                Text(movie.Title)
+                    .font(
+                        .system(size: 34, weight: .semibold, design: .serif)
+                    )
                 
-                    Text("6.9/10 ⭐️")
+                Text("6.9/10 ⭐️")
                 
-                    Text("Hentai")
-                        .padding()
-                
-                    HStack(spacing: 24) {
-                        Button { store.toggleLike(for: movie) } label: {
-                            Image(
-                                systemName: store
-                                    .isLiked(movie) ? "heart.fill" : "heart"
-                            )
-                            .foregroundStyle(
-                                store.isLiked(movie) ? .red : .secondary
-                            )
-                            .font(.system(size: 28))
-                        }
-                        Button { store.toggleWatchLater(for: movie) } label: {
-                            Image(
-                                systemName: store
-                                    .isWatchLater(
-                                        movie
-                                    ) ? "plus.app.fill" : "plus.app"
-                            )
-                            .foregroundStyle(
-                                store.isWatchLater(movie) ? .yellow : .secondary
-                            )
-                            .font(.system(size: 28))
-                        }
-                        Button { } label: {
-                            Image(systemName: "play.circle.fill")
-                                .foregroundStyle(.secondary)
-                                .font(.system(size: 28))
-                        }
-                        .disabled(true)
-                    }
+                Text("Hentai")
                     .padding()
-                }
-                else {
-                    VStack {
-                        Image(systemName: "wifi.slash")
-                        Text("Check your Wifi Connection!")
+                
+                HStack(spacing: 24) {
+                    Button { store.toggleLike(for: movie) } label: {
+                        Image(
+                            systemName: store
+                                .isLiked(movie) ? "heart.fill" : "heart"
+                        )
+                        .foregroundStyle(
+                            store.isLiked(movie) ? .red : .secondary
+                        )
+                        .font(.system(size: 28))
                     }
+                    Button { store.toggleWatchLater(for: movie) } label: {
+                        Image(
+                            systemName: store
+                                .isWatchLater(
+                                    movie
+                                ) ? "plus.app.fill" : "plus.app"
+                        )
+                        .foregroundStyle(
+                            store.isWatchLater(movie) ? .yellow : .secondary
+                        )
+                        .font(.system(size: 28))
+                    }
+                    Button { } label: {
+                        Image(systemName: "play.circle.fill")
+                            .foregroundStyle(.secondary)
+                            .font(.system(size: 28))
+                    }
+                    .disabled(true)
                 }
-            
+                .padding()
+            }
+            else {
+                VStack {
+                    Image(systemName: "wifi.slash")
+                    Text("Check your Wifi Connection!")
+                }
+            }
         }
     }
 }
